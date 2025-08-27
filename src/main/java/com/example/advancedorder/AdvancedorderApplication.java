0dad26c60d8a12eb68a5cbb1dc1d81dc85b02294
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2025-08-28 00:23:28 +0530

    Advanced Spring Boot: order service: intial setup

---
+++ src/main/java/com/example/advancedorder/AdvancedorderApplication.java
@@ -0,0 +1,13 @@
+package com.example.advancedorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/advancedorder/AdvancedorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advancedorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
